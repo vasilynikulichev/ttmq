25b0287279bba63e628910145113bbc8f15f897c
VasilyNikulichev <vasilynikulichev@gmail.com>
2021-05-27 23:45:33 +0300

    add polyfills

--- configs/webpack/common.js
+++ configs/webpack/common.js
@@ -1,10 +1,11 @@
 const paths = require('../paths');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     context: paths.src,
-    entry: './index.js',
+    entry: ['whatwg-fetch', 'custom-event-polyfill', './index.js'],
     output: {
         filename: 'bundle.min.js',
         path: paths.build,
@@ -19,14 +20,14 @@ module.exports = {
                     options: {
                         presets: [['@babel/preset-env', {
                             corejs : {
-                                version : "3",
+                                version : '3',
                                 proposals : true
                             },
                             useBuiltIns: 'usage',
                             targets: {
                                 browsers: [
-                                    "last 2 version",
-                                    "ie >= 11"
+                                    'last 2 version',
+                                    'ie >= 11'
                                 ]
                             }
                         }]]
@@ -40,5 +41,8 @@ module.exports = {
             template: './index.html'
         }),
         new CleanWebpackPlugin(),
+        new webpack.ProvidePlugin({
+            'Promise': 'es6-promise',
+        })
     ]
 };
